ffd760e18695c0420eac2ec15e4c20aeb69009c0
Hải My <myhainguyen02@gmail.com>
2024-11-08 18:53:09 +0700

    sửa lại hiển thị Phí ship

--- giohang.php
+++ giohang.php
@@ -46,6 +46,7 @@ if (isset($_SESSION["user"])) {
         echo "<div style='text-align: center; margin-top: 20px; font-weight: bold;'>Tổng tiền: <span id='tongtien'>0 VNĐ</span></div>";
         echo "<script>tinhTongTien();</script>"; // Tính tổng tiền khi tải trang
         echo "<div style='text-align: center; margin-top: 20px; font-weight: bold;'>VAT: <span id='VAT'>0 VNĐ</span></div>";
+        echo "<div style='text-align: center; margin-top: 20px; font-weight: bold;'>Phí vận chuyển: <span id='shippingCost'>0 VNĐ</span></div>";
         echo "<div style='text-align: center; margin-top: 20px; font-weight: bold;'>Thành tiền: <span id='thanhTienTong'>0 VNĐ</span></div>";
 
         echo "<input type='hidden' value='$i' name='slmahang'>";
@@ -301,8 +302,8 @@ $con->close();
             <option value="Yên Bái">Yên Bái</option>
         </select><br>
         Địa chỉ:<input type="text" name="diachi" required><br>
-        <label for="shippingCost">Phí vận chuyển:</label>
-        <span id="shippingCost">0 VNĐ</span><br>
+        <!-- <label for="shippingCost">Phí vận chuyển:</label>
+        <span id="shippingCost">0 VNĐ</span><br> -->
         <input type='hidden' name='vat' value="0">
         <input type='hidden' name='tongtien' value="0">
         <input type='hidden' name='shipping' value="0">
